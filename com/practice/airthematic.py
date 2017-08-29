d7f2be326b8ca7888139491ad5db0a97fb4aeb16
Sandeep Kumar Singh <esasisa@users.noreply.github.com>
2017-08-29 18:11:59 +0530

    formatting
    formatting

--- com/practice/airthematic.py
+++ com/practice/airthematic.py
@@ -11,7 +11,8 @@ def is_leap(year):
         if(year % 100 == 0):
             leap = False
         if(year % 400 == 0):
-            leap = True  
+            leap = True
+			
     return leap
 
 if __name__ == '__main__': 
